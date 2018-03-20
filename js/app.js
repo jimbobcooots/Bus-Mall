e5a4d2fdef3a1adcdf0502e55ddfd9c23d106c88
Noah Israel <noahmvf@gmail.com>
2018-03-20 13:45:43 -0700

    added pseudo-code currently working through random image function

--- js/app.js
+++ js/app.js
@@ -3,35 +3,69 @@
 //array that will hold each Picture
 Picture.allPictures = [];
 
+//Here we access our image elements from the DOM
+var imgElementOne = document.getElementById('pic-one');
+var imgElementTwo = document.getElementById('pic-two');
+var imgElementThree = document.getElementById('pic-three');
+
 function Picture(filepath, name) {
   this.filepath = filepath;
   this.name = name;
   Picture.allPictures.push(this);
 }
 
+//These are our image instances
 new Picture('assets/bag.jpg', 'Bag');
 new Picture('assets/banana.jpg', 'Banana');
 new Picture('assets/bathroom.jpg', 'Bathroom');
 new Picture('assets/boots.jpg', 'Boots');
+new Picture('assets/breakfast.jpg', 'Breakfast');
+new Picture('assets/bubblegum.jpg', 'Bubblegum');
+new Picture('assets/chair.jpg', 'Chair');
+new Picture('assets/cthulhu.jpg', 'Cthulhu');
+new Picture('assets/dog-duck.jpg', 'Dog-Duck');
+new Picture('assets/dragon.jpg', 'Dragon');
+new Picture('assets/pen.jpg', 'Pen');
+new Picture('assets/pet-sweep.jpg', 'Pet-Sweep');
+new Picture('assets/scissors.jpg', 'Scissors');
+new Picture('assets/shark.jpg', 'Shark');
+new Picture('assets/sweep.jpg', 'Sweep');
+new Picture('assets/tauntaun.jpg', 'Tauntaun');
+new Picture('assets/unicorn.jpg', 'Unicorn');
+new Picture('assets/usb.jpg', 'USB');
+new Picture('assets/water-can.jpg', 'Water-Can');
+new Picture('assets/wine-glass.jpg', 'Wine-Glass');
 
-var imgElementOne = document.getElementById('pic-one');
+//we want to randomly display three of our instances
+function threeRandomImages() {
+  var randomOne= Math.floor(Math.random()Picture.allPictures.length);
 
-var imgElementTwo = document.getElementById('pic-two');
+  var randomTwo = Math.floor(Math.random()Picture.allPictures.length);
 
-var imgElementThree = document.getElementById('pic-three');
+  var randomThree = Math.floor(Math.random()Picture.allPictures.length);
+
+  //we are establishing three random number variables
+  //They are generating a number between 0 and 1, mulitplying that num by the length of our instances array, then rounding the num
+
+  //Now we have three random images, but there is a chance they could be the same num
+
+  //we also want to set these variables equal to our imgElement variables within our HTML doc
+  
+}
 
 function randomPicture() {
   //we want three numbers to be drawn from the indexes of our Picture.allPictures array 
   var indexArray = [];
-  for (var count = 0; count < 3; count++) {
+  for (var i = 0; i < 3; i++) {
     var randomIndex = Math.floor(Math.random() * Picture.allPictures.length);
   
     indexArray.push(randomIndex);
     console.log(indexArray);
+    
+    imgElementOne.src = Picture.allPictures[indexArray[i]].filepath;
+    imgElementOne.alt = Picture.allPictures[indexArray[i]].name;
 
-    while (count < 3) {
-      imgElementOne.src = Picture.allPictures[indexArray[count]].filepath;
-      imgElementOne.alt = Picture.allPictures[indexArray[count]].name;
+     
       //while the count is less than 3, set imgElement.src to the filepath in Picture.allPictures to be an index of our filepaths contained by a random num in our indexArray
     }
   }
